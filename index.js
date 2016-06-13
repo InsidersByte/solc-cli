52615fb087005449932a3ecb167901591a0178ae
Jonathon Kelly <jonathon@insidersbyte.com>
2016-06-13 09:43:46 +0100

    add debug messages and use base name of file instead of filename

--- index.js
+++ index.js
@@ -14,37 +14,61 @@ cli.parse({
 });
 
 cli.main((args, { 'out-dir': outputDirectory, optimise }) => {
+    cli.debug(`out-dir: ${outputDirectory}`);
+    cli.debug(`args: ${args}`);
+
+    if (optimise) {
+        cli.debug('solc optimiser enabled');
+    }
+
     if (args.length === 0) {
         cli.fatal('a file is required');
     }
 
     const currentPath = process.cwd();
 
+    cli.debug(`currentPath: ${currentPath}`);
+
     const sources = {};
 
     for (const fileName of args) {
+        cli.debug(`loading ${fileName}`);
+
+        const baseName = path.basename(fileName);
         const filePath = path.join(currentPath, fileName);
         const file = fs.readFileSync(filePath, 'utf8');
 
-        sources[fileName] = file;
+        sources[baseName] = file;
+
+        cli.debug(`finished loading ${fileName}`);
     }
 
     const outputDirectoryPath = path.join(currentPath, outputDirectory);
 
-    mkdirp(outputDirectoryPath, (error) => {
-        if (error) {
-            throw error;
-        }
+    cli.debug(`out-dir-path ${outputDirectoryPath}`);
 
-        const compiledContracts = solc.compile({ sources }, optimise);
+    mkdirp.sync(outputDirectoryPath);
 
-        if (compiledContracts.errors) {
-            throw compiledContracts.errors;
-        }
+    cli.debug('out-dir created (or it already existed)');
 
-        for (const [key, value] of Object.entries(compiledContracts.contracts)) {
-            fs.writeFile(path.join(outputDirectoryPath, `${key}.bin`), value.bytecode);
-            fs.writeFile(path.join(outputDirectoryPath, `${key}.abi`), value.interface);
-        }
-    });
+    cli.debug('compiling contracts');
+
+    const compiledContracts = solc.compile({ sources }, optimise);
+
+    cli.debug('finished compiling contracts');
+
+    if (compiledContracts.errors) {
+        throw compiledContracts.errors;
+    }
+
+    cli.debug('compiled without any errors');
+
+    for (const [key, value] of Object.entries(compiledContracts.contracts)) {
+        cli.debug(`creating ${key}.bin and ${key}.abi`);
+
+        fs.writeFile(path.join(outputDirectoryPath, `${key}.bin`), value.bytecode);
+        fs.writeFile(path.join(outputDirectoryPath, `${key}.abi`), value.interface);
+
+        cli.debug(`finished creating ${key}.bin and ${key}.abi`);
+    }
 });
